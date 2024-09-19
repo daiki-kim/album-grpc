29bc72420b63870b5428dec2c764f8c7fc75a3f6
daiki-kim <kimdaiki5021@gmail.com>
2024-09-20 07:50:47 +0900

    fix logging and timeout

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"album-grpc/pb"
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"time"
@@ -11,22 +12,28 @@ import (
 )
 
 var (
+	serverAddr = "localhost:50051"
+
 	timeoutDuration = 10 * time.Second
-	serverAddr      = "localhost:50051"
+	timeSleep       = 0 * time.Second
 )
 
+// Unary RPCの実装例
+// サーバーにtitleを送り、ファイルに存在するかの確認結果をAlbum型で受け取る
 func callGetAlbum(client pb.AlbumServiceClient, title string) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
 	defer cancel()
 
-	album, err := client.GetAlbum(ctx, &pb.GetAlbumRequest{Title: title})
+	resp, err := client.GetAlbum(ctx, &pb.GetAlbumRequest{Title: title})
 	if err != nil {
 		log.Fatalf("client.GetAlbum failed: %v", err)
 	}
 
-	log.Println(album)
+	log.Printf("response: %v", resp.Album)
 }
 
+// ServerStreaming RPCの実装例
+// サーバーにartistを送り、artistが一致するAlbumをすべてAlbum型で受け取る
 func callListAlbums(client pb.AlbumServiceClient, artist string) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
 	defer cancel()
@@ -37,7 +44,7 @@ func callListAlbums(client pb.AlbumServiceClient, artist string) {
 	}
 
 	for {
-		album, err := stream.Recv()
+		resp, err := stream.Recv()
 		if err == io.EOF {
 			break
 		}
@@ -45,10 +52,12 @@ func callListAlbums(client pb.AlbumServiceClient, artist string) {
 			log.Fatalf("client.ListAlbums failed: %v", err)
 		}
 
-		log.Println(album)
+		log.Printf("response: %v", resp.Album)
 	}
 }
 
+// ClientStreaming RPCの実装例
+// サーバーに複数のtitleを送り、ファイルに存在したAlbumの総数とその合計金額、メッセージを受け取る
 func callGetTotalAmount(client pb.AlbumServiceClient) {
 	titles := []string{
 		"Blue Train",
@@ -71,6 +80,8 @@ func callGetTotalAmount(client pb.AlbumServiceClient) {
 		if err := stream.Send(&pb.GetTotalAmountRequest{Title: title}); err != nil {
 			log.Fatalf("client.GetTotalAmount: stream.Send(%s) failed: %v", title, err)
 		}
+
+		time.Sleep(timeSleep)
 	}
 
 	resp, err := stream.CloseAndRecv()
@@ -78,21 +89,88 @@ func callGetTotalAmount(client pb.AlbumServiceClient) {
 		log.Fatalf("client.GetTotalAmount: stream.CloseAndRecv failed: %v", err)
 	}
 
-	log.Println(resp)
+	log.Printf("response: %v", resp)
+}
+
+// BidirectionalStreaming RPCの実装例
+// サーバーにアップロードしたいAlbumを連続で送り、その度にサーバーからメッセージを受け取る
+func callUploadAndNotify(client pb.AlbumServiceClient) {
+	albums := []*pb.Album{
+		{Title: "New Album", Artist: "New Artist", Price: 10.99},
+		{Title: "New Album 2", Artist: "New Artist 2", Price: 20.99},
+		{Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+		{Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
+	defer cancel()
+
+	stream, err := client.UploadAndNotify(ctx)
+	if err != nil {
+		log.Fatalf("client.UploadAndNotify failed: %v", err)
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for {
+			recp, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+
+			if err != nil {
+				log.Fatalf("client.UploadAndNotify: stream.Recv() failed: %v", err)
+			}
+
+			log.Printf("response: %v", recp)
+		}
+	}()
+
+	for _, album := range albums {
+		req := &pb.UploadAndNotifyRequest{Album: album}
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("client.UploadAndNotify: stream.Send(%v) failed: %v", album, err)
+		}
+
+		time.Sleep(timeSleep)
+	}
+
+	if err := stream.CloseSend(); err != nil {
+		log.Fatalf("client.UploadAndNotify: stream.CloseSend() failed: %v", err)
+	}
+	<-waitc
 }
 
 func main() {
-	conn, err := grpc.NewClient(serverAddr, grpc.WithInsecure())
+	conn, err := grpc.NewClient(serverAddr, grpc.WithInsecure()) // gRPCクライアントを作成
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer conn.Close()
-	client := pb.NewAlbumServiceClient(conn)
+	client := pb.NewAlbumServiceClient(conn) // gRPCクライアントをAlbumServiceClientに変換
 
+	// Unary RPCの実行例
+	fmt.Println("Unary RPC started")
+	fmt.Println("valid request")
 	callGetAlbum(client, "Blue Train")
+	fmt.Println("invalid request")
 	callGetAlbum(client, "Not Exist Title")
+	fmt.Println("")
 
+	// ServerStreaming RPCの実行例
+	fmt.Println("ServerStreaming RPC started")
 	callListAlbums(client, "Miles Davis")
+	fmt.Println("")
 
+	// ClientStreaming RPCの実行例
+	fmt.Println("ClientStreaming RPC started")
 	callGetTotalAmount(client)
+	fmt.Println("")
+
+	// BidirectionalStreaming RPCの実行例
+	fmt.Println("BidirectionalStreaming RPC started")
+	callUploadAndNotify(client)
+	fmt.Println("")
 }
--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -16,18 +17,27 @@ import (
 var (
 	filePath = "db/album.json"
 	port     = "50051"
+
+	timeSleep = 0 * time.Second
 )
 
+// pb.AlbumServiceServerインターフェースを満たすサーバーを定義する
+// 未実装のメソッドはpb.UnimplementedAlbumServiceServerのメソッドが使用される
 type AlbumServer struct {
 	pb.UnimplementedAlbumServiceServer
 
 	savedAlbums []*pb.Album
 }
 
+// Unary RPCの実装例
+// クライアントからtitleを受け取り、ファイルにデータが存在するかの確認結果をAlbum型で返す
 func (s *AlbumServer) GetAlbum(ctx context.Context, req *pb.GetAlbumRequest) (*pb.GetAlbumResponse, error) {
+	fmt.Println("")
+	fmt.Println("Unary RPC started")
+
+	log.Printf("request: %s", req.Title)
 	for _, album := range s.savedAlbums {
 		if album.Title == req.Title {
-			log.Printf("album found: %s", req.Title)
 			return &pb.GetAlbumResponse{Album: album}, nil
 		}
 	}
@@ -36,21 +46,31 @@ func (s *AlbumServer) GetAlbum(ctx context.Context, req *pb.GetAlbumRequest) (*p
 	return &pb.GetAlbumResponse{Album: &pb.Album{}}, nil
 }
 
+// ServerStreaming RPCの実装例
+// クライアントからartistを受け取り、artistが一致するAlbumをすべてAlbum型で返す
 func (s *AlbumServer) ListAlbums(req *pb.ListAlbumsRequest, stream pb.AlbumService_ListAlbumsServer) error {
+	fmt.Println("")
+	fmt.Println("ServerStreaming RPC started")
+
+	log.Printf("request: %s", req.Artist)
 	for _, album := range s.savedAlbums {
 		if album.Artist == req.Artist {
-			log.Printf("%s's album found: %s", req.Artist, album.Title)
 			if err := stream.Send(&pb.ListAlbumsResponse{Album: album}); err != nil {
 				return err
 			}
+			time.Sleep(timeSleep) // 確認用
 		}
 	}
 
-	log.Printf("%s's album not found", req.Artist)
 	return nil
 }
 
+// ClientStreaming RPCの実装例
+// クライアントから複数のtitleを受け取り、ファイルに存在したAlbumの総数とその合計金額、メッセージを返す
 func (s *AlbumServer) GetTotalAmount(stream pb.AlbumService_GetTotalAmountServer) error {
+	fmt.Println("")
+	fmt.Println("ClientStreaming RPC started")
+
 	var (
 		albumCount  int32
 		totalAmount float32
@@ -76,13 +96,53 @@ func (s *AlbumServer) GetTotalAmount(stream pb.AlbumService_GetTotalAmountServer
 		log.Printf("request: %s", req.Title)
 		for _, album := range s.savedAlbums {
 			if album.Title == req.Title {
-				log.Printf("%s's price: %f", req.Title, album.Price)
 				totalAmount += album.Price
+				break
 			}
 		}
 	}
 }
 
+// BidirectionalStreaming RPCの実装例
+// クライアントから複数のAlbumを受け取り、その度にメッセージを返す
+// 受け取ったAlbumがファイルに存在しない場合はファイルに追加する
+func (s *AlbumServer) UploadAndNotify(stream pb.AlbumService_UploadAndNotifyServer) error {
+	fmt.Println("")
+	fmt.Println("BidirectionalStreaming RPC started")
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("request: %s", req.Album.Title)
+		res := &pb.UploadAndNotifyResponse{}
+
+		for _, album := range s.savedAlbums {
+			if album.Title == req.Album.Title {
+				res.Message = fmt.Sprintf("%s is already exists", req.Album.Title)
+				break
+			}
+		}
+
+		if res.Message == "" {
+			s.savedAlbums = append(s.savedAlbums, req.Album)
+			s.UpdateAlbums(s.savedAlbums, filePath)
+			res.Message = fmt.Sprintf("%s is uploaded", req.Album.Title)
+		}
+
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+// JSONファイルからAlbumのリストをロードするメソッド
 func (s *AlbumServer) loadAlbums(filePath string) error {
 	var (
 		data []byte
@@ -100,6 +160,28 @@ func (s *AlbumServer) loadAlbums(filePath string) error {
 	return nil
 }
 
+// AlbumのリストをJSONファイルに書き込むメソッド
+func (s *AlbumServer) UpdateAlbums(albums []*pb.Album, filePath string) error {
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	newData, err := json.MarshalIndent(albums, "", "	")
+	if err != nil {
+		return err
+	}
+
+	_, err = newFile.Write(newData)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// サーバーを作成するメソッド
 func newServer() *AlbumServer {
 	s := &AlbumServer{}
 	s.loadAlbums(filePath)
@@ -114,10 +196,10 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterAlbumServiceServer(grpcServer, newServer())
+	pb.RegisterAlbumServiceServer(grpcServer, newServer()) // 作成したサーバーをgrpcServerに登録
 
 	log.Println("server started")
-	if err := grpcServer.Serve(lis); err != nil {
+	if err := grpcServer.Serve(lis); err != nil { // grpcServerを起動
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
